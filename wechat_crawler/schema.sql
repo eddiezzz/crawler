42d93b4e0de01a85bead60a8975115065e3dda83
郑长帅 <zhengchangshuai@mobike.com>
2018-11-01 01:43:06 +0800

    update

--- wechat_crawler/schema.sql
+++ wechat_crawler/schema.sql
@@ -9,7 +9,8 @@ CREATE TABLE `wechat_mp_crawl` (
   `_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
   `wechat_name` varchar(50) DEFAULT '' COMMENT '要添加的公众号名称',
   `wechat_id` varchar(50) DEFAULT '' COMMENT '公众号的微信号',
-  PRIMARY KEY (`_id`)
+  PRIMARY KEY (`_id`),
+  UNIQUE KEY (`wechat_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `wechat_mp_profile`;
