028a91863be1ab32f86a7b0c1df08c03808db5ca
eddiezzz <zcs5612@163.com>
2018-11-02 22:20:16 +0800

    update

---
+++ wechat_crawler/reset_db.sql
@@ -0,0 +1,19 @@
+source schema.sql
+insert into wechat_mp_crawl(wechat_id) values('cailianpress');
+insert into wechat_mp_crawl(wechat_id) values('caixinwang');
+insert into wechat_mp_crawl(wechat_id) values('financeapp');
+insert into wechat_mp_crawl(wechat_id) values('geekpark');
+insert into wechat_mp_crawl(wechat_id) values('hejqianyan');
+insert into wechat_mp_crawl(wechat_id) values('hkguruclub');
+insert into wechat_mp_crawl(wechat_id) values('HRresearch88');
+insert into wechat_mp_crawl(wechat_id) values('huxiu_com');
+insert into wechat_mp_crawl(wechat_id) values('kejiguancha ');
+insert into wechat_mp_crawl(wechat_id) values('MSRAsia');
+insert into wechat_mp_crawl(wechat_id) values('QbitAI');
+insert into wechat_mp_crawl(wechat_id) values('qiyejiagc');
+insert into wechat_mp_crawl(wechat_id) values('rmrbwx');
+insert into wechat_mp_crawl(wechat_id) values('RUC_NADS');
+insert into wechat_mp_crawl(wechat_id) values('shangjiexinmeiti');
+insert into wechat_mp_crawl(wechat_id) values('techcrunchcn');
+insert into wechat_mp_crawl(wechat_id) values('WebNotes');
+insert into wechat_mp_crawl(wechat_id) values('wow36kr');
